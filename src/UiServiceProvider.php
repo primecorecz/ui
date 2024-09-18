b4f5bf8329b566dbe7f91d5613cab4eceade3b02
Vn <vitnasinec@gmail.com>
2024-09-18 11:10:51 +0200

    Register views

--- src/UiServiceProvider.php
+++ src/UiServiceProvider.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
+namespace Primecorecz\Ui;
+
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class UiServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'primecore');
+        Blade::componentNamespace('Primecorecz\\Ui\\View\\Components', 'primecore');
+    }
+
     public function boot(): void
     {
-        dd('boot');
     }
 }
