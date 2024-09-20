fed6ab8ac93d0de28ba60651ae9f47e2aea4808f
Vn <vitnasinec@gmail.com>
2024-09-20 13:26:40 +0200

    Adjust header text size

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -8,10 +8,10 @@
 
     <div class="md:min-h-[20rem] grid place-content-center">
         <div class="my-8 mx-4 flex flex-col lg:flex-row gap-y-4 gap-x-8 items-center font-heading text-white">
-            <a href="/" class="lg:pr-8 lg:pb-2 lg:border-r-2 border-white text-5xl md:text-6xl font-bold">
+            <a href="/" class="lg:pr-8 lg:pb-2 lg:border-r-2 border-white text-4xl sm:text-5xl md:text-6xl font-bold">
                 {{ $name }}<span class="opacity-50">.cz</span>
             </a>
-            <span class="text-2xl text-center">
+            <span class="text:xl sm:text-2xl text-center">
                 {{ $slogan }}
             </span>
         </div>
