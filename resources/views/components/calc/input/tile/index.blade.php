79b0780f4e8bee023f7829df1a723f27c0601da2
Vn <vitnasinec@gmail.com>
2024-09-20 13:27:46 +0200

    Normalize input tile height

--- resources/views/components/calc/input/tile/index.blade.php
+++ resources/views/components/calc/input/tile/index.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->class('p-6 rounded-md shadow-lg bg-white') }}>
+<div {{ $attributes->class('min-h-[4.5rem] p-4 sm:p-6 sm:min-h-[5.5rem] rounded-md shadow-lg bg-white') }}>
     {{ $slot }}
 </div>
