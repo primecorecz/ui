88b328160d3bf12ba39a63e9dc9793eadd14d8f2
Vn <vitnasinec@gmail.com>
2024-09-29 13:27:09 +0200

    Add padding to header container

--- resources/views/components/header/container.blade.php
+++ resources/views/components/header/container.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->class('md:min-h-[20rem] py-8 grid place-content-center') }}>
+<div {{ $attributes->class('md:min-h-[20rem] py-8 px-4 grid place-content-center') }}>
     {{ $slot }}
 </div>
