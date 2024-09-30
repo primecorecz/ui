696792db22819d46f8337281cc678ebf7d01a122
Vn <vitnasinec@gmail.com>
2024-09-30 11:17:55 +0200

    Fix tabs resizing on page load

--- resources/views/components/tabs/item.blade.php
+++ resources/views/components/tabs/item.blade.php
@@ -2,5 +2,5 @@
 <a {{ $attributes->class([
     'inline-block py-4 lg:py-6 px-4 md:px-8 lg:px-12',
     'font-bold text-sm md:text-base lg:text-lg',
-    'transition-all duration-500 ease-in-out',
+    'transition-color duration-500 ease-in-out',
 ]) }} >{{ $slot }}</a>
